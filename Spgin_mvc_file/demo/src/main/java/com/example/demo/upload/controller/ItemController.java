b330fd716fe9c0062d363ce042c72a1a01ca1573
ggong00 <ggo0415@naver.com>
2022-09-04 23:50:52 +0900

    파일 업로드, 다움로드 구현

---
+++ Spgin_mvc_file/demo/src/main/java/com/example/demo/upload/controller/ItemController.java
@@ -0,0 +1,63 @@
+package com.example.demo.upload.controller;
+
+import com.example.demo.upload.domain.Item;
+import com.example.demo.upload.domain.ItemRepository;
+import com.example.demo.upload.domain.UploadFile;
+import com.example.demo.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+    @PostMapping("/items/new")
+    public String saveItem(@ModelAttribute ItemForm form, RedirectAttributes
+            redirectAttributes) throws IOException {
+
+        log.info("itemForm {}", form);
+
+        UploadFile attachFile = fileStore.storeFile(form.getAttachFile());
+        List<UploadFile> storeImageFiles =
+                fileStore.storeFiles(form.getImageFiles());
+
+        //데이터베이스에 저장
+        Item item = new Item();
+        item.setItemName(form.getItemName());
+        item.setAttachFile(attachFile);
+        item.setImageFiles(storeImageFiles);
+        itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+
+        log.info("item {}", item);
+        log.info("fileName {}", item.getAttachFile().getStoreFileName());
+
+        return "redirect:/items/{itemId}";
+    }
+
+    @GetMapping("/items/{id}")
+    public String items(@PathVariable Long id, Model model) {
+        Item item = itemRepository.findById(id);
+        model.addAttribute("item", item);
+        return "item-view";
+    }
+}
